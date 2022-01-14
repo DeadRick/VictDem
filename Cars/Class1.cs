214b278c50d41b1a410b490c48ad3a486665ab24
DeadRick <vndemyanenko@edu.hse.ru>
2022-01-14 22:16:37 +0300

    Ex2 Cars

--- Cars/Class1.cs
+++ Cars/Class1.cs
@@ -2,8 +2,48 @@
 
 namespace Cars
 {
-    public class Class1
+    public delegate void CarEngineHandler(string msgForCaller);
+    
+    public class Car
     {
-        
+        // Информация о внутреннем состоянии
+        private CarEngineHandler listOfHandlers;
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+        // Машина работоспособна?
+        private bool carIsDead;
+        // Конструкторы
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall) { listOfHandlers = methodToCall; }
+        public void Accelerate(int delta)
+        {
+            // Если машина сломана, отправляем оповещение.
+            if (carIsDead)
+            {
+                listOfHandlers?.Invoke("К сожалению, машина сломана :( ...");
+                return;
+            }
+
+            CurrentSpeed += delta;
+
+            // Машина почти сломана?
+            if (MaxSpeed > CurrentSpeed && (MaxSpeed - CurrentSpeed) <= 10)
+                listOfHandlers?.Invoke("Предупреждение! Будь осторожнее");
+
+            // Машина сломана?
+            if (CurrentSpeed >= MaxSpeed)
+                carIsDead = true;
+            else
+                Console.WriteLine("Скорость = {0}", CurrentSpeed);
+        }
+
     }
+
 }
--- Delegate ExCar/Program.cs
+++ Delegate ExCar/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using Cars;
 
 namespace Delegate_ExCar
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("What!");
-            Console.WriteLine("OneTwo!");
+            Console.WriteLine("***** Использование делегатов для управления событиями *****\n");
+
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Передаём в машину метод, который будет вызван при отправке оповещения.
+            c1.RegisterWithCarEngine(new Cars.CarEngineHandler(OnCarEngineEvent));
+            // Разгоняем машину
+            Console.WriteLine("***** Увеличиваем скорость *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            Console.ReadLine();
         }
+        // Это метод-обработчик оповещений от машины.
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Сообщение от объекта типа Car *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***********************************\n");
+        }
+
     }
 }
