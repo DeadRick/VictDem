8b0058c47b07ad830e61201a9e8ac783549dd969
DeadRick <vndemyanenko@edu.hse.ru>
2022-01-19 11:36:48 +0300

    first Task is done

---
+++ Delegate MySelf1 p2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegate_MySelf1_p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-15, 16);
+            }
+
+            Console.WriteLine("Unsorted array: ");
+
+            foreach (var it in arr)
+            {
+                Console.Write(it + " ");
+            }
+
+            Array.Sort(arr, (x, y) => x.CompareTo(y));
+
+            Console.WriteLine("\nSotred array with lambda expression: ");
+
+            foreach (var it in arr)
+            {
+                Console.Write(it + " ");
+            }
+        }
+    }
+}
