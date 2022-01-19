4aa408b5fcde2b3a4eda2491aee7ecd565270030
DeadRick <vndemyanenko@edu.hse.ru>
2022-01-19 11:51:58 +0300

    second Task is done (Delegate p2)

---
+++ Delegate MySelf2 p2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Delegate_MySelf2_p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new();
+            int[] arrA = new int[10];
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                arrA[i] = rnd.Next(1, 21);
+            }
+
+            double[] arrB;
+            arrB = Array.ConvertAll<int, double>(arrA, item => 1.0/item);
+
+            Console.WriteLine("Array A: ");
+            foreach (var it in arrA)
+            {
+                Console.Write(it + " ");
+            }
+
+            Console.WriteLine("\nArray B (1/x): ");
+            foreach (var it in arrB)
+            {
+                Console.Write(it.ToString("f2") + " ");
+            }
+        }
+    }
+}
