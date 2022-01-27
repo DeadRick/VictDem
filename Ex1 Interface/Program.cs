881eeeffdf52caa7e916a99db110ba5b63595213
DeadRick <vndemyanenko@edu.hse.ru>
2022-01-27 19:33:07 +0300

    - the fourth seminar

---
+++ Ex1 Interface/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Ex1_Interface
+{
+    public interface ISeries
+    { // интерфейс числовых рядов
+
+        void SetBegin();            // задать начальное состояние
+        int GetNext { get; }        // вернуть очередной член ряда
+        int this[int k] { get; }    // вернуть к-й член ряда
+    }
+    public class Pell : ISeries
+    { // Ряд Пелла: 1, 2, 5, 12,...      
+        int old, last;              // два предыдущих члена ряда
+        public Pell() { SetBegin(); }   // конструктор
+        public void SetBegin()
+        {// задать начальное состояние
+            old = 0; last = 1;
+        }
+        public int GetNext
+        {    // вернуть следующий после last         
+            get
+            {
+                int now = old + 2 * last;
+                old = last; last = now;
+                return now;
+            }
+        }
+        public int this[int k]
+        { // вернуть к-й член ряда        
+            get
+            {
+                int now = 0;
+                SetBegin();
+                if (k <= 0) return -1;
+                for (int j = 0; j < k; j++) now = GetNext;
+                return now;
+            }
+        }
+    } // end of Pell
+
+    public class Fibok : ISeries
+    { // Ряд Пелла: 1, 2, 5, 12,...      
+        int old, last;              // два предыдущих члена ряда
+        public Fibok() { SetBegin(); }   // конструктор
+        public void SetBegin()
+        {// задать начальное состояние
+            old = 0; last = 2;
+        }
+        public int GetNext
+        {    // вернуть следующий после last         
+            get
+            {
+                int now = old + last;
+                old = last; last = now;
+                return now;
+            }
+        }
+        public int this[int k]
+        { // вернуть к-й член ряда        
+            get
+            {
+                int now = 0;
+                SetBegin();
+                if (k <= 0) return -1;
+                for (int j = 0; j < k; j++) now = GetNext;
+                return now;
+            }
+        }
+    } // end of Pell
+
+    public class Luke : ISeries
+    { // Ряд Пелла: 1, 2, 5, 12,...      
+        int old, last;              // два предыдущих члена ряда
+        public Luke() { SetBegin(); }   // конструктор
+        public void SetBegin()
+        {// задать начальное состояние
+            old = 3; last = =1;
+        }
+        public int GetNext
+        {    // вернуть следующий после last         
+            get
+            {
+                int now = old + last;
+                old = last; last = now;
+                return now;
+            }
+        }
+        public int this[int k]
+        { // вернуть к-й член ряда        
+            get
+            {
+                int now = 0;
+                SetBegin();
+                if (k <= 0) return -1;
+                for (int j = 0; j < k; j++) now = GetNext;
+                return now;
+            }
+        }
+    } // end of Pell
+
+    class Program
+    {
+        // вывести n членов ряда
+        public static void SeriesPrint(int n, ISeries s)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(s.GetNext + "\t");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Pell pell = new Pell();
+            SeriesPrint(9, pell);
+            Console.WriteLine("pell[3] = " + pell[3]);
+            SeriesPrint(4, pell);
+            SeriesPrint(3, pell);
+
+        }
+    }
+
+}
---
+++ Ex2 Interface/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Ex2_Interface
+{
+    interface ITransformable
+    {   // интерфейс преобразование
+        void Transform(double coef);    // преобразовать 
+    }
+    class Circle : ITransformable
+    {   // круг
+        double rad = 1;     // радиус круга
+        public void Transform(double coef) { rad *= coef; }
+        public override string ToString()
+        {
+            return String.Format("Площадь круга: {0:G4}",
+                              Math.PI * rad * rad);
+        }
+    }
+    class Piramida : ITransformable
+    {
+        // Треугольник
+        public double B { get; set; } = 10;
+        public double H { get; set; } = 10;
+
+
+        public void Transform(double coef)
+        {
+            B *= coef;
+            H *= coef;
+        }
+
+        public override string ToString()
+        {
+            return $"Объём пирамиды: {B * B * 1.0/3.0 * H}" + Environment.NewLine + $"Площадь боковой поверхности - S";
+        }
+    }
+
+    class Cube : ITransformable
+    {   // куб
+        double rib = 1;     // ребро куба
+        public void Transform(double coef) { rib *= coef; }
+        public override string ToString()
+        {
+            return String.Format("Объем куба: {0:G4}",
+                             rib * rib * rib);
+        }
+    }
+
+    class Cylinder : ITransformable
+    {
+        public double R { get; set; } = 10;
+        public double H { get; set; } = 10;
+        public void Transform(double coef)
+        {
+            R *= coef;
+            H *= coef;
+        }
+        public override string ToString()
+        {
+            return $"{Math.PI * R * R * H}";
+        }
+    }
+
+    class Function : ITransformable
+    {
+        public double K { get; set; } = 1;
+
+        public void Transform(double coef)
+        {
+            K *= coef;
+        }
+
+        public override string ToString()
+        {
+            return $"{K}";
+        }
+    }
+
+    class Program
+    {
+        public static void Report(ITransformable g)
+        {
+            Console.WriteLine("Данные объекта класса {0}:", g.GetType());
+            Console.WriteLine(g);
+        }
+        public static ITransformable Mapping(ITransformable g, double d)
+        {
+            g.Transform(d);
+            return g;
+        }
+        public static void Main()
+        {
+            ITransformable[] arr = { new Cube(), new Cylinder(), new Piramida(), new Circle(), new Function() };
+            int k = int.Parse(Console.ReadLine());
+
+            Array.ForEach(arr, x => {
+                x.Transform(k);
+                Report(x);
+                });
+
+
+        }
+
+
+    }
+
+}
+
