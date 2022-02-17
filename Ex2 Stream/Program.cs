f8ebe92088731f796d734b1657a63ad13966acc2
DeadRick <vndemyanenko@edu.hse.ru>
2022-02-17 15:23:34 +0300

    - Stream  - - Ex2

--- Ex2 Stream/Program.cs
+++ Ex2 Stream/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
-namespace Ex2_Stream
+using System.IO;
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        FileInfo fi = new FileInfo(@"Alphabet.txt");
+        Console.WriteLine("Choose a letter which will be replace with * sym");
+        string deletedSym = Console.ReadLine();
+        using (FileStream fs = fi.Open(FileMode.OpenOrCreate))
         {
-            Console.WriteLine("Hello World!");
+            long len = fs.Length;   // Размер файла
+            if (len == 26) Console.WriteLine("Aлфавит собран!");
+            else
+            {
+                if (len == 0) Console.WriteLine("Файл пуст!");
+                fs.Seek(len, SeekOrigin.Begin);
+                byte bt = (byte)('A' + len);
+                fs.WriteByte(bt);
+                Console.WriteLine("Добавляем в файл букву " + (char)bt);
+            }
+            Console.WriteLine("Буквы в файле:");
+            fs.Seek(0, SeekOrigin.Begin);
+            int u;
+            while ((u = fs.ReadByte()) != -1) {
+                if ((char)u == deletedSym[0])
+                {
+                    Console.Write("*" + "  ");
+                }
+                else
+                {
+                    Console.Write((char)u + "  ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
